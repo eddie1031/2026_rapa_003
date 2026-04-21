02bdcbe951e085149abd2db682048bd7cd3703b8
eddie1031 <archaco123@gmail.com>
2026-04-21 16:57:59 +0900

    n+1

--- src/main/java/mapper/s4/MemberMapper.java
+++ src/main/java/mapper/s4/MemberMapper.java
@@ -4,9 +4,12 @@ import domain.Member;
 import domain.MemberDto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberMapper {
 
     List<MemberDto> findAll();
+
+    Optional<Member> findById(Long id);
 
 }

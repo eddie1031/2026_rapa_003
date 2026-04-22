9c212dde09f82bf9e4a526ff4088c62da080e330
eddie1031 <archaco123@gmail.com>
2026-04-22 11:33:22 +0900

    first lv cache

---
+++ src/main/java/CacheTests.java
@@ -0,0 +1,114 @@
+import domain.Product;
+import mapper.s5.ProductMapper;
+import org.apache.ibatis.session.SqlSession;
+import util.HikariMyBatisConnector;
+import util.MyBatisSessionConnector;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class CacheTests {
+
+    private static final String MAPPER_PACKAGE = "mapper.s5";
+    private static final String ALIAS_PACKAGE = "domain";
+
+    private static final MyBatisSessionConnector connector = new HikariMyBatisConnector(
+            MAPPER_PACKAGE,
+            ALIAS_PACKAGE
+    );
+
+    void main() {
+//        test1();
+        test2();
+    }
+
+    void test1() {
+
+        System.out.println("CacheTests.test1");
+
+        try (SqlSession session = connector.openSession()) {
+
+            ProductMapper mapper = session.getMapper(ProductMapper.class);
+
+            long targetId = 1L;
+
+            System.out.println("첫 번째 product 조회 시도");
+            Optional<Product> productOptional1 = mapper.findById(targetId);
+            productOptional1.ifPresent(p -> System.out.println("상품이 조회되었습니다!"));
+
+            System.out.println("두 번째 product 조회 시도");
+            Optional<Product> productOptional2 = mapper.findById(targetId);
+            productOptional2.ifPresent(p -> System.out.println("상품이 조회되었습니다!"));
+
+            System.out.println("product1과 product2 비교");
+            System.out.println(productOptional1.get() == productOptional2.get());
+
+        }
+
+        try (SqlSession session = connector.openSession()) {
+
+            ProductMapper mapper = session.getMapper(ProductMapper.class);
+
+            long targetId = 1L;
+
+            System.out.println("세 번째 product 조회 시도");
+            Optional<Product> productOptional1 = mapper.findById(targetId);
+            productOptional1.ifPresent(p -> System.out.println("상품이 조회되었습니다!"));
+
+
+        }
+
+
+
+
+    }
+
+    void test2() {
+
+        System.out.println("CacheTests.test2");
+
+        try (SqlSession session = connector.openSession()) {
+
+            ProductMapper mapper = session.getMapper(ProductMapper.class);
+
+            long targetId = 1L;
+
+            System.out.println("첫 번째 product 조회 시도");
+            Optional<Product> productOptional1 = mapper.findById(targetId);
+            productOptional1.ifPresent(p -> System.out.println("상품이 조회되었습니다!"));
+
+            System.out.println("두 번째 product 조회 시도");
+            Optional<Product> productOptional2 = mapper.findById(targetId);
+            productOptional2.ifPresent(p -> System.out.println("상품이 조회되었습니다!"));
+
+            System.out.println("product1과 product2 비교");
+            System.out.println(productOptional1.get() == productOptional2.get());
+
+            Product product1 = productOptional1.get();
+            Product product2 = productOptional2.get();
+
+            System.out.println("product1 과 product2의 동일성 검사" + (product1 == product2));
+
+            System.out.println("상품 재고 업데이트 시도");
+            mapper.updateById(
+                    new Product(
+                            product1.getId(),
+                            product1.getName(),
+                            product1.getPrice(),
+                            100
+                    )
+            );
+
+            System.out.println("세 번째 product 조회 시도");
+            Optional<Product> productOptional3 = mapper.findById(targetId);
+            productOptional2.ifPresent(p -> System.out.println("상품이 조회되었습니다!"));
+            Product product3 = productOptional3.get();
+            System.out.println("product1 과 product3의 동일성 검사" + (product1 == product3));
+
+
+
+        }
+
+    }
+
+}
--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -20,3 +20,4 @@ public class Order {
     private List<OrderItem> orderItems;
 
 }
+
